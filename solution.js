82a1ec09d9c8612ad30ebd01335915b7f4476f14
Laura Wu <leemaiwu@gmail.com>
2023-07-12 14:08:48 -0600

    Progress

--- solution.js
+++ solution.js
@@ -1,3 +1,11 @@
 const powerOf4 = (n) => {
-  
+  if (n === Math.pow(4, (Math.log(n) / Math.log(4)))) {
+    return true
+  }
+  return false
 }
+
+console.log(powerOf4(4)) // true
+console.log(powerOf4(44)) // false
+console.log(powerOf4(1024)) // true
+console.log(powerOf4('1024')) // false
